477a7fb57d505f7d6f6c12ff33387cb0f2ed0671
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-09-22 16:55:25 +0200

    Adding style

--- cards/src/Pages/CardCreate.tsx
+++ cards/src/Pages/CardCreate.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../Styles/addcards.css';
 
 const CardCreate = () => {
   const [title, setTitle] = useState('');
@@ -57,30 +58,60 @@ const CardCreate = () => {
 
   return (
     <div>
-      <h1>Create a New Card</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
+      <div className='create-new-card'>
+        <div className='create-new-card-div'>
+          <div className='create-new-card-div-second'>
+            <h1 className='main-text-gradient-card-create'>Create A New Card</h1>
+            <div className='form-card-creation'>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <div className="wave-group wave-group-one">
+                    <input 
+                      type="text"
+                      id="title"
+                      value={title}
+                      className='input'
+                      onChange={(e) => setTitle(e.target.value)}
+                      required 
+                    />
+                    <span className="bar"></span>
+                    <label className="label">
+                      <span className="label-char" style={{ '--index': 0 } as React.CSSProperties}>C</span>
+                      <span className="label-char" style={{ '--index': 1 } as React.CSSProperties}>a</span>
+                      <span className="label-char" style={{ '--index': 2 } as React.CSSProperties}>r</span>
+                      <span className="label-char" style={{ '--index': 3 } as React.CSSProperties}>d</span>
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <div className="wave-group wave-group-second">
+                    <input 
+                      type="text"
+                      id="title"
+                      value={response}
+                      className='input'
+                      onChange={(e) => setResponse(e.target.value)}
+                      required 
+                    />
+                    <span className="bar"></span>
+                    <label className="label">
+                      <span className="label-char" style={{ '--index': 0 } as React.CSSProperties}>R</span>
+                      <span className="label-char" style={{ '--index': 1 } as React.CSSProperties}>e</span>
+                      <span className="label-char" style={{ '--index': 2 } as React.CSSProperties}>s</span>
+                      <span className="label-char" style={{ '--index': 3 } as React.CSSProperties}>p</span>
+                      <span className="label-char" style={{ '--index': 4 } as React.CSSProperties}>o</span>
+                      <span className="label-char" style={{ '--index': 5 } as React.CSSProperties}>n</span>
+                      <span className="label-char" style={{ '--index': 6 } as React.CSSProperties}>s</span>
+                      <span className="label-char" style={{ '--index': 7 } as React.CSSProperties}>e</span>
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" className='create-card-button'>Create Card</button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="response">Response:</label>
-          <input
-            type="text"
-            id="response"
-            value={response}
-            onChange={(e) => setResponse(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Create Card</button>
-      </form>
+      </div>
 
       <h2>Existing Cards</h2>
       <ul>
