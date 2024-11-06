30796ea4873e17c6d8dd782f90a7ad02f8ad4ef5
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-11-06 21:37:22 +0100

    +

--- cards/src/datas/devoirs.py
+++ cards/src/datas/devoirs.py
@@ -7,16 +7,3 @@ def contient_voyelle(mot):
         if lettre in voyelles:
             return True
     return False
-
-def derniere_consonne(mot):
-    voyelles = "aeiouy"
-    for i in range(len(mot) - 1, -1, -1):
-        if mot[i] not in voyelles:
-            return i, mot[i]
-    return None, None
-
-def double_consonne(mot):
-    voyelles = "aeiouy"
-    for i in range(len(mot) - 1):
-        if mot[i] == mot[i + 1] and mot[i] not
-    return mot not in mots_interdits
