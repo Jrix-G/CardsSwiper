d2be2a07b94ab18acdc57a43a45d21d6d30d490b
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-11-12 15:48:39 +0100

    +

--- cards/src/datas/devoirs.py
+++ cards/src/datas/devoirs.py
@@ -3,4 +3,24 @@ def commence_par(lettre, mot):
 
 def contient_voyelle(mot):
     voyelles = "aeiouy"
+    for lettre in mot:    return mot[0] == lettre
+
+def contient_voyelle(mot):
+    voyelles = "aeiouy"
+    for lettre in mot:    return mot[0] == lettre
+
+def contient_voyelle(mot):
+    voyelles = "aeiouy"
+    for lettre in mot:    return mot[0] == lettre
+
+def contient_voyelle(mot):
+    voyelles = "aeiouy"
+    for lettre in mot:    return mot[0] == lettre
+
+def contient_voyelle(mot):
+    voyelles = "aeiouy"
+    for lettre in mot:    return mot[0] == lettre
+
+def contient_voyelle(mot):
+    voyelles = "aeiouy"
     for lettre in mot:
