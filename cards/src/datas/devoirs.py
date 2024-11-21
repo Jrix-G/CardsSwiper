9177066f7e7e485d039fd017ad37f59d9417a25e
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-11-21 18:10:31 +0100

    +

--- cards/src/datas/devoirs.py
+++ cards/src/datas/devoirs.py
@@ -1,30 +1,73 @@
-def prix_menu(nom_menu, avecBoisson=False, nb_supplement=0):
-    prix = 0
-    if nom_menu == "Basique":
-        prix = 9
-    elif nom_menu == "Gourmand":
-        prix = 15
-    elif nom_menu == "Complet":
-        prix = 19
-    else:
-        raise ValueError("Nom de menu invalide")
+def recherche_dicho(liste, e):
+    left = 0
+    right = len(liste) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if liste[mid] < e:
+            left = mid + 1
+        elif liste[mid] > e:
+            right = mid - 1
+        else:
+            return liste
 
-    if avecBoisson:
-        prix += 4
+    liste.append(0)
+    for i in range(len(liste) - 1, left, -1):
+        liste[i] = liste[i - 1]
+    liste[left] = e
+    return liste
 
-    prix += nb_supplement * 1.5
+import random
+def list_creation(n):
+    liste = []
+    for i in range(n):
+        liste.append(random.randint(0, 1000))
+    return liste
 
-    return prix
+import time
+"""
+start_time = time.time()
+print(recherche_dicho(list_creation(1000000), 1000))
+second_time = time.time()
+print("Temps dicho", second_time - start_time)
+"""
 
-def table_Dupont():
-    prix_jacqueline = prix_menu("Basique")
-    prix_michel = prix_menu("Gourmand", avecBoisson=True)
-    prix_johanna = prix_menu("Basique", nb_supplement=2)
-    prix_antoine = prix_menu("Basique", avecBoisson=True, nb_supplement=1)
+def test(liste):
+    if len(liste) <= 1:
+        return liste
+    mid = len(liste) // 2
+    left = test(liste[:mid])
+    right = test(liste[mid:])
+    return fusion(left, right)
 
-    total = prix_jacqueline + prix_michel + prix_johanna + prix_antoine
-    return total
+def fusion(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])    
+    return result
 
-# Exemple d'utilisation
-if __name__ == "__main__":
-    print(f"Le prix total pour la table des Dupont est de {table_Dupont()} euros.")
+
+time_un = time.time()
+print(test(list_creation(10**1)))
+time_deux = time.time()
+print("temps", time_deux - time_un)
+
+def affichage_console():
+    i = 0
+    result = "#"
+    current_time = time.time()
+    while i < 100:
+        if current_time % 2 == 0:
+            print(result + "2", end="")
+            result += "#"
+        else:
+            print(result + "5", end="")
+            result += "#"
+        i += 1
