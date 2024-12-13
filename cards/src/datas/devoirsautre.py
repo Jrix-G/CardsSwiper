801061d43481c0618b1a6558b6d6dd633964c85b
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-12-13 20:54:49 +0100

    Small changes

--- cards/src/datas/devoirsautre.py
+++ cards/src/datas/devoirsautre.py
@@ -1,2 +1,21 @@
-def moyenne_etudiant(etudiant):
-    moyenne = 0
+def dico_add(dico, key, value):
+    if key in dico:
+        dico[key].append(value)
+    else:
+        dico[key] = value
+    return dico
+
+def dico_search(dico, value):
+    for key in dico:
+        print(key)
+        if dico[key] == value:
+            return key, value
+    return None
+
+def calcul_marche(dico, marche="marche"):
+    for key in dico:
+        if key == marche:
+            sume = 0
+            for i in dico[key]:
+                sume += dico[key][i]
+    return sume
