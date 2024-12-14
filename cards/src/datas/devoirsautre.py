5dcf69f1efa2c58f318dcddf506d68403f7de338
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-12-14 21:04:58 +0100

    +

--- cards/src/datas/devoirsautre.py
+++ cards/src/datas/devoirsautre.py
@@ -1,21 +1,23 @@
-def dico_add(dico, key, value):
-    if key in dico:
-        dico[key].append(value)
+def isPrime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def primeFactors(n):
+    if isPrime(n):
+        return [n]
     else:
-        dico[key] = value
-    return dico
-
-def dico_search(dico, value):
-    for key in dico:
-        print(key)
-        if dico[key] == value:
-            return key, value
-    return None
-
-def calcul_marche(dico, marche="marche"):
-    for key in dico:
-        if key == marche:
-            sume = 0
-            for i in dico[key]:
-                sume += dico[key][i]
-    return sume
+        factors = []
+        i = 2
+        while i * i <= n:
+            if n % i:
+                i += 1
+            else:
+                n //= i
+                factors.append(i)
+        if n > 1:
+            factors.append(n)
+        return factors
+    
+print(primeFactors(18))
