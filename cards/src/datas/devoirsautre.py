0a4f2af029bb60c686c5bf0412d07d5b7ae89ce4
Jason <64320121+Jrix-G@users.noreply.github.com>
2024-12-15 14:13:46 +0100

    +

--- cards/src/datas/devoirsautre.py
+++ cards/src/datas/devoirsautre.py
@@ -3,21 +3,3 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
-
-def primeFactors(n):
-    if isPrime(n):
-        return [n]
-    else:
-        factors = []
-        i = 2
-        while i * i <= n:
-            if n % i:
-                i += 1
-            else:
-                n //= i
-                factors.append(i)
-        if n > 1:
-            factors.append(n)
-        return factors
-    
-print(primeFactors(18))
